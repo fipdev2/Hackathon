b4819f59d0e331b6c5dd08fb29f24fd2d2bffb8e
Vinícius Lima <villylss@gmail.com>
2022-09-03 17:08:15 -0300

    estrutura da tela onboarding com gif

---
+++ front/hackaton-app/src/pages/OnboardingGif/style.tsx
@@ -0,0 +1,42 @@
+import styled from 'styled-components/native';
+
+export const ScreenContainer = styled.View`
+    background-image: linear-gradient(180deg, #068888 0%, #02556D 100%);
+    height: 100%;
+`
+
+export const GifView = styled.View`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: -30%;
+`
+
+export const TurtleGif = styled.Image`
+    border: 4px solid #02566D;
+    border-bottom-right-radius: 200px;
+    border-bottom-left-radius: 200px;
+`
+
+export const TextView = styled.View`
+    padding: 0 6%;
+    margin-top: -8%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+export const Title = styled.Text`
+    font-size: 25%;
+    font-weight: 700;
+    color: #fff;
+    margin-bottom: 5%;
+`
+
+export const Description = styled.Text`
+    font-size: 15%;
+    color: #fff;
+    text-align: center;
+`
+
+
